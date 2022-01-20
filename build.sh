998aa1097dcf63e5b228fde30939b35d18e9b68e
Akhil Kumar <kumaakh@users.noreply.github.com>
2022-01-19 21:11:57 -0500

    nproc -2 does not work on a small 2 proc machine

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir -p _build
 cd _build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_DIR -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_WRITERS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BLACKBOX_TESTS=OFF ../
-cmake --build . -- -j "$(($(nproc) - 2))"
+cmake --build . -- -j "$(($(nproc) - 1))"
 
 make install
 
